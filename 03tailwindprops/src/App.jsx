9673bbcdcb800b2aeb3f6ccd5dd9dc2b65efb541
Codesorcer <Rudraprataps7789@gmail.com>
2024-12-06 18:43:19 +0530

    project 3 - tailwind props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,33 @@
+//import { useState } from "react";
+//import reactLogo from "./assets/react.svg";
+//import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+//  const [count, setCount] = useState(0);
+
+let myObj = {
+  name: 'Rudra',
+  age: 25,
+  address: {
+    city: 'Bikaner',
+    state: 'Rajasthan'
+  }
+}
+
+let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  return (
+    <>
+      <h1 className="text-3xl bg-green-500 p-3 rounded-md">
+        Vite with Tailwind
+      </h1>
+      <Card username="Rudra" myArr={newArr} />
+      <Card username='Json' post='Staff Engg.' />
+      <Card />
+    </>
+  );
+}
+
+export default App;
